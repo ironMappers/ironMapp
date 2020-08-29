de888ae7972c4ae55aa455226889fe2dda798345
TL Garcia <tomas.bl.garcia@gmail.com>
2020-08-29 11:19:10 +0200

    minor: Corrected routes spelling and centered view content

--- configs/routes.js
+++ configs/routes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const uploads = multer({dest: './public/uploads'})
+const uploads = multer({dest: './public/uploads'});
 const passport = require('passport');
 
-const session = require('../middlewares/session.middleware')
+const session = require('../middlewares/session.middleware');
 
 const miscController = require('../controllers/misc.controller');
-const usersController = require('../controllers/users.controller')
+const usersController = require('../controllers/users.controller');
 
 router.get('/', miscController.renderHome);
-router.get('/login', usersController.login);
-router.get('/signup', usersController.signup);
+router.get('/login', usersController.renderLogin);
+router.get('/signup', usersController.renderSignup);
 
 module.exports = router;
