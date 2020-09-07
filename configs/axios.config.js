18ea1047353b20e94471ef4dcdaa8544b09accb4
TL Garcia <tomas.bl.garcia@gmail.com>
2020-09-07 19:15:37 +0200

    checkpoint

--- configs/axios.config.js
+++ configs/axios.config.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const apiData = () => {
-    return axios.get('https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres')
+    return axios.get('https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres');
 };
 
 module.exports.apiData = apiData;
--- controllers/stations.controller.js
+++ controllers/stations.controller.js
@@ -1,13 +1,15 @@
-
 const axiosConfig = require('../configs/axios.config');
 
 module.exports.renderStation = (req, res, next) => {
-    const id = req.params.id
+    const id = req.params.id;
+
     Promise.all([axiosConfig.apiData()])
         .then(response => {
-            const stationsArr = response[0].data.ListaEESSPrecio
-            res.send(stationsArr.filter(el => el.IDEESS === id))
+            const stationsArr = response[0].data.ListaEESSPrecio;
+            const station = stationsArr.filter(st => st.IDEESS === id)[0];
+            console.log(station);
+            //const {name, price} = {station['Rótulo'], }
+            res.render('stations/details', {station});
         })
-        .catch(e => console.error(e))
-
+        .catch(e => console.error(e));
 };
