28867549744464128dcc06993f037aae76d045da
TL Garcia <tomas.bl.garcia@gmail.com>
2020-09-08 12:49:13 +0200

    checkpoint

--- configs/axios.config.js
+++ configs/axios.config.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const apiData = () => {
-    return axios.get('https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres')
+    return axios.get('https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroProvincia/05')
 };
 
 module.exports.apiData = apiData;
