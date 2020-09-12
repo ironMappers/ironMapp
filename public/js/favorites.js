232861993aac80ad11c405b68468c88f3dc1af31
Tomas <58976333+TL-Garcia@users.noreply.github.com>
2020-09-12 04:14:52 -0400

    Station/crud (#13)
    * task: Enable CRUD of favorite station, station rating, reviews in the station-details view

---
+++ public/js/favorites.js
@@ -0,0 +1,31 @@
+const favoriteBtn = document.getElementById('btn-favorite');
+
+const renderFavorite = () => {
+    if (favoriteBtn.value === 'true') {
+        favoriteBtn.innerHTML = '<i class="fa fa-heart"></i>';
+    } else {
+        favoriteBtn.innerHTML = '<i class="fa fa-heart-o"></i>';
+    }
+};
+
+const invertBoolean = (value) => {
+    if (value === 'true') {
+        return 'false';
+    } 
+    return 'true';
+};
+
+favoriteBtn.addEventListener('click', event => {
+   favoriteBtn.value = invertBoolean(favoriteBtn.value);
+   renderFavorite();
+    axios.put(`${APP_URL}/favorite`, {
+       station: {
+           district ,
+           IDEESS: id
+       }      
+    })   
+        .then(favorite => console.log(favorite))
+        .catch(e => console.error(e));
+});
+
+renderFavorite();
