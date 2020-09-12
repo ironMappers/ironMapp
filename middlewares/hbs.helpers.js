0e4f76746f3c01554d1d98b3b4802bb5d736c153
TL Garcia <tomas.bl.garcia@gmail.com>
2020-09-12 23:36:34 +0200

    task: Format reviews' creation time

--- middlewares/hbs.helpers.js
+++ middlewares/hbs.helpers.js
@@ -7,3 +7,16 @@ hbs.registerHelper('owner', (userId, elementId, options) => {
     } 
     return options.inverse(this);
 });
+
+hbs.registerHelper('date', (date) => {
+    //const makeDoubleDigit = (num) => (num < 10) ? '0' + num : num;
+    const makeDoubleDigit = (fullDate) => fullDate.split(' ').map(num => (num.length < 2) ? '0' + num : num);
+    const d = new Date(date)
+  const fullDate =  `${date.getDate()} ${date.getMonth() + 1} ${date.getFullYear()}`;
+  const time = `${date.getHours()} ${date.getMinutes()}`;
+  const formattedTime = makeDoubleDigit(time).join(':');
+  const formattedDate = makeDoubleDigit(fullDate).join('/');
+
+  console.log(formattedDate)
+    return `${formattedDate} at ${formattedTime}`;
+});
